f40edfb1e8b215dad88fb54bae7c52511fcd5015
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2022-12-06 11:36:35 +0100

    add Google Analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LETDWXE4J3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LETDWXE4J3');
+    </script>
+
+
+    <title>Short URL - MintPHP.dev</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
