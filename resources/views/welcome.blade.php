a2ba5b7caa01a522f233bfdb9dce2ad6c0fb4261
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2022-12-06 11:35:45 +0100

    add ability to see readme on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             <div class="flex flex-col items-center w-full pr-5 lg:w-1/3 lg:flex-row">
                 <p class="w-full mb-5 text-base text-center text-green-600 lg:text-left lg:w-48 lg:mb-0"
                    data-primary="green-400">An OS Software by <br>Marco Introini</p>
-                <a href="#_" class="flex-shrink-0 px-4 py-3 text-white bg-green-500 border-green-700"
+                <a href="https://marcointroini.it" class="flex-shrink-0 px-4 py-3 text-white bg-green-500 border-green-700"
                    data-primary="green-400" data-rounded="">Visit Home Page</a>
             </div>
             <div class="hidden w-2/3 lg:block"></div>
@@ -86,7 +86,15 @@
             </h5>
 
         </div>
-        <div class="flex h-3/4 w-full bg-gray-100 rounded-md"></div>
+        <div class="flex h-3/4 w-full rounded-md">
+            <div class="prose lg:prose-xl">
+                <x-markdown>
+                    @php(readfile('../README.md'))
+                </x-markdown>
+
+            </div>
+
+        </div>
     </div>
 </section>
 
