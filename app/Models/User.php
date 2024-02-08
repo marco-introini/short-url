dc20985a5daeaac8faa5c60cc164d485395b53ee
Marco "Mint" Introini <mint.dev@pm.me>
2024-02-08 20:49:38 +0100

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
